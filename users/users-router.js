3840edeecad7ca6bae270419e2ccebe5093cf5a0
jambis <imjaamees@gmail.com>
2019-11-18 23:30:42 -0600

    users endpoints

--- users/users-router.js
+++ users/users-router.js
@@ -1,20 +1,12 @@
 const router = require("express").Router();
 
-router.get("/:id", async (req, res) => {
+const dbUsers = require("./users-model");
+
+router.get("/:uid", async (req, res) => {
   //
   res.end();
 });
 
-router.post("/:id/vacation/", async (req, res) => {
-  //
-});
-
-router.put("/:id/vacation/:id", async (req, res) => {
-  //
-});
-
-router.delete("/:id/vacation/:id", async (req, res) => {
-  //
-});
+//create friendlist
 
 module.exports = router;
