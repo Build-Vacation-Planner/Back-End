bf23b8e754447c29fedbc1b23ae8351592bfa586
jambis <imjaamees@gmail.com>
2019-11-18 23:31:07 -0600

    vacation users endpoints

---
+++ vacations/users/users-router.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+
+router.post("/", async (req, res) => {
+  //enter username
+  //insert user id into user_vacations
+});
+
+router.delete("/:id", async (req, res) => {
+  //delete username with id
+});
+
+module.exports = router;
