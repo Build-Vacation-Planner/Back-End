30f82ddb9372c20b6ab6a8be130018818eefe83d
jambis <imjaamees@gmail.com>
2019-11-20 11:49:38 -0600

    seeds

--- data/seeds/05-dates.js
+++ data/seeds/05-dates.js
@@ -1,6 +1,7 @@
 exports.seed = function(knex) {
   return knex("dates").insert([
     { start: "2020-03-08", end: "2020-03-15", vacation_id: 1 },
-    { start: "2019-12-20", end: "2019-12-27", vacation_id: 2 }
+    { start: "2019-12-20", end: "2019-12-27", vacation_id: 2 },
+    { start: "2019-12-20", end: "2019-12-27", vacation_id: 3 }
   ]);
 };
