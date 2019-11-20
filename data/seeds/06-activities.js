5f0dc9419d3a993cd3cd2d7ecc8509fdef3b04d2
jambis <imjaamees@gmail.com>
2019-11-19 22:07:13 -0600

    Corrected spelling error

--- data/seeds/06-activities.js
+++ data/seeds/06-activities.js
@@ -3,7 +3,7 @@ exports.seed = function(knex) {
     { name: "Drink", vacation_id: 1 },
     { name: "Drink again", vacation_id: 1 },
     { name: "Drink some more", vacation_id: 1 },
-    { name: "Sskiing", vacation_id: 2 },
+    { name: "Skiing", vacation_id: 2 },
     { name: "Gambling", vacation_id: 2 },
     { name: "Clubbing", vacation_id: 2 },
     { name: "Xcaret", description: "Wild life tour", vacation_id: 3 }
